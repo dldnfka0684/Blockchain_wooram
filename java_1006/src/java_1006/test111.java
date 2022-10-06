6e1f08e0b5e7407a63022278b912d41876631f04
tjoeun <tjoeun@DESKTOP-PA5EOVA>
2022-10-06 18:04:12 +0900

    1006

---
+++ java_1006/src/java_1006/test111.java
@@ -0,0 +1,25 @@
+package java_1006;
+
+public class test111 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<5;i=i+1)
+		{
+			if(i<3) {
+				for(int j=0;j<i+1;j=j+1)
+				{
+					System.out.println("*");
+				}else {
+					for(int j=0;j<5-i;j=j+1)
+					{
+						System.out.println("*");
+					}
+				}
+			}
+			System.out.println("");
+		}
+	}
+
+}
+
